79aea27c969ccb69c9e31aca573593e902a8e31d
ppplllil-spec <banseun36@gmail.com>
2026-02-10 02:13:23 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,16 @@
-// [주의] 이 파일은 절대로 GitHub에 올리지 마세요! (.gitignore 등록 필수)
 const CONFIG = {
-    // 1. 구글 클라우드 콘솔에서 새로 발급받은 API 키
-    // (메일에서 지적된 기존 키: AIzaSyDZQL2iDN220bBdU0xiYDShqLRRp6AQJDI 는 삭제 후 새 키를 넣으세요!)
+    // 1. 공통 API 키 (구글 시트 & Firebase 공용)
     API_KEY: "AIzaSyB6r7cGl5np3FhKsB_wDnwNGbS14RfoCE4",
 
-    // 2. 구글 콘솔 '사용자 인증 정보' 탭의 OAuth 2.0 클라이언트 ID
-    CLIENT_ID: "776703554625-v64h7180436isb6p67o7n6f1qsc0907v.apps.googleusercontent.com",
+    // 2. 구글 서비스 관련 설정
+    GOOGLE: {
+        CLIENT_ID: "776703554625-v64h7180436isb6p67o7n6f1qsc0907v.apps.googleusercontent.com",
+        SHEET_ID: "1O4M_r_ZMNFOCRPlIuqMpgfwRtdUSAwY520lyrBX104Y",
+        GAS_URL: "https://script.google.com/macros/s/AKfycbwsaZ9xNvPdZENLmjS-V0sWy22ItvR43YAr3b1K2qTkHrfjUgSedbqnAVpQ2hdc040T/exec"
+    },
 
-    // 3. 연동하신 구글 시트의 고유 ID
-    SPREADSHEET_ID: "1_6U4Y16v_U4lK9h2H7G7h7V8-V8_v8_V8v8V8v8V8v8" 
-};
-const API_KEY = "AIzaSyB6r7cGl5np3FhKsB_wDnwNGbS14RfoCE4";
-    const SHEET_ID = "1O4M_r_ZMNFOCRPlIuqMpgfwRtdUSAwY520lyrBX104Y";
-    const GAS_URL = "https://script.google.com/macros/s/AKfycbwsaZ9xNvPdZENLmjS-V0sWy22ItvR43YAr3b1K2qTkHrfjUgSedbqnAVpQ2hdc040T/exec";
-    
-const firebaseConfig = {
+    // 3. 파이어베이스 관련 설정
+    FIREBASE: {
         apiKey: "AIzaSyB6r7cGl5np3FhKsB_wDnwNGbS14RfoCE4",
         authDomain: "chat-9035a.firebaseapp.com",
         databaseURL: "https://chat-9035a-default-rtdb.asia-southeast1.firebasedatabase.app",
@@ -23,4 +19,5 @@ const firebaseConfig = {
         messagingSenderId: "807179401789",
         appId: "1:807179401789:web:644359473a0cb1e05f4854",
         measurementId: "G-BKQNKVRB8X"
-    };
+    }
+};
